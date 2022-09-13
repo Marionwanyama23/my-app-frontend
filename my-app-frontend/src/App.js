d080858e0ff2f04d8cf0568e722898f8536c21da
+ <+>
2022-09-13 10:55:25 +0300

    commit

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -1,6 +1,83 @@
-function App() {
+import LandingPage from "./Components/LandingPage";
+import MainPage from "./Components/MainPage";
+import CreateRoutineForm from "./Components/CreateRoutineForm";
+import CreateExerciseForm from "./Components/CreateExerciseForm";
+import React, {useEffect, useState } from "react";
+import {Route, useHistory} from "react-router-dom";
+
+
+function App() {  
+
+  const potato = (data) => data.json()
+  const [routines, setRoutines] = useState([])
+  const [exercises, setExercise] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/routines")
+    .then(potato)
+    .then(setRoutines)
+  }, [])
+
+  const history = useHistory()
+
+  const handleAddRoutine = (newRoutine) => {
+    fetch('http://localhost:9292/routines', {
+      method:'POST',
+      headers: {
+          'Content-Type':'application/json',
+      },
+      body: JSON.stringify(newRoutine)
+    }) 
+    .then(potato)
+    .then(newRoutineFromBackend => {
+        const updatedRoutines =  [newRoutineFromBackend, ...routines]
+        setRoutines(updatedRoutines)
+        history.push("/mainpage")
+    })
+}
+
+const handleAddExercise = (newExercise) => {
+  fetch('http://localhost:9292/exercises', {
+      method:'POST',
+      headers: {
+          'Content-Type':'application/json',
+      },
+      body: JSON.stringify(newExercise)
+  }) 
+  .then(potato)
+  .then(newExerciseFromBackend => {
+    const updatedExercises =  [newExerciseFromBackend, ...exercises]
+    setExercise(updatedExercises)
+    history.push("/mainpage")
+  })
+}
+
+const handleDelete = (id) => {
+  fetch(`http://localhost:9292/routines/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-type': 'application/json'
+    },
+  })
+  .then(potato)
+  .then(setRoutines)
+}
+
   return (
-    <h1>hello world</h1>
+    <>
+      <Route exact path = "/">
+        <LandingPage />
+      </Route>
+      <Route exact path = "/mainpage">
+        <MainPage routines={routines} handleDelete={handleDelete}/>
+      </Route>
+      <Route exact path= "/newexercise">
+        <CreateExerciseForm handleAddExercise={handleAddExercise}/>
+      </Route>
+      <Route exact path= "/newroutine">
+        <CreateRoutineForm handleAddRoutine={handleAddRoutine}/>    
+      </Route>
+    </>
   );
 }
 
